492cf69e6b29759c138b2a253d770f952bb28932
mallik-user1 <42467608+mallik-user1@users.noreply.github.com>
2021-12-01 15:11:35 +0530

    Update main.tf

--- terraform/aws/modules/network/main.tf
+++ terraform/aws/modules/network/main.tf
@@ -3,7 +3,8 @@ data "aws_availability_zones" "available" {}
 # Create a VPC to launch our instances into
 #b newc 
 resource "aws_vpc" "km_vpc" {
-  #ZS:SKIP=AC_AWS_0369 ignore this policy
+  #zs:skip=AC_AWS_0369 ignore this policy
+  
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
