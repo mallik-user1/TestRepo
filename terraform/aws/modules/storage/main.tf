aedaf68d57acfd35b7ea3327c0f778b272bcc624
mallik-user1 <42467608+mallik-user1@users.noreply.github.com>
2021-12-02 15:39:13 +0530

    Update main.tf

--- terraform/aws/modules/storage/main.tf
+++ terraform/aws/modules/storage/main.tf
@@ -32,6 +32,7 @@ resource "aws_security_group" "km_rds_sg" {
   }
 }
 
+# check regression
 resource "aws_kms_key" "km_db_kms_key" {
   description             = "KMS Key for DB instance ${var.environment}"
   deletion_window_in_days = 10
